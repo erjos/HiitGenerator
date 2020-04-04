1ab00870039ba9e2c3ee1fd49b0ed8b6d1b82faa
Ethan Joseph <ethanrj91@gmail.com>
2020-04-03 21:09:58 -0700

    remove file add .gitignore

--- HiitGenerator/WorkoutGenerator.swift
+++ HiitGenerator/WorkoutGenerator.swift
@@ -5,10 +5,10 @@
 //  Created by Ethan Joseph on 12/18/19.
 //  Copyright © 2019 Ethan Joseph. All rights reserved.
 //
-
 import Foundation
+import FirebaseFirestore
 
-//these enums help define workout preferences
+// Not sure if I actually need these right now
 enum WOLength : Int {
     case short = 20
     case medium = 40
@@ -23,32 +23,5 @@ enum WOComponents {
 }
 
 class WOGenerator {
-    //workout preferences
-    var length : WOLength!
-    var components : WOComponents!
-    var targetAreas : [TargetArea] = [.Abs,.Arms,.Back,.Biceps,.Cardio,.Chest,.Core,.Glutes,.Hamstring,.Legs,.Obliques,.Quads,.Shoulders,.Triceps]
-    
-    lazy var workoutGetter : WorkoutStorageProtocol = WorkoutDataModels()
-    
-    func generateWorkout(_ exclude: [TargetArea], _ workoutLength: WOLength, _ components: WOComponents) {
 
-        let validWorkouts = workoutGetter.getWorkouts()
-            .filter { $0.targetAreas
-                .first { exclude
-                    .contains($0)
-                } == nil
-        }
-        
-        // randomly pick one from the list
-        let firstWorkout = validWorkouts.randomElement()
-        
-        //update workout pool and pull another one
-    }
-    
-    //If we know the types of  workouts we want to incorporate we can have partitioned lists ahead of time so that we dont need to resort or whatever...
-    
-    //init()
-    
-    //probably dont need this right away but would be nice later on
-    //have the ability to customize this later on - add remove workouts that the user doesnt want or like
 }
