9f31f5ace97eca929e47d73bd8581e2545c2f8f8
Ethan Joseph <ethanrj91@gmail.com>
2020-04-04 21:25:42 -0700

    setup crm vc

--- HiitGenerator/CreateExerciseViewController.swift
+++ HiitGenerator/CreateExerciseViewController.swift
@@ -9,11 +9,33 @@
 import UIKit
 
 class CreateExerciseViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    @IBOutlet weak var InstructionsOne: UITextField!
+    @IBOutlet weak var InstructionsTwo: UITextField!
+    @IBOutlet weak var InstructionsThree: UITextField!
+    @IBOutlet weak var cardioSwitch: UISwitch!
+    @IBOutlet weak var armsSwitch: UISwitch!
+    @IBOutlet weak var legsSwitch: UISwitch!
+    @IBOutlet weak var coreSwitch: UISwitch!
+    @IBOutlet weak var difficultyField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didPressSave(_ sender: Any) {
+        //Validate all the fields and save the new workout
+    }
+    
+    @IBAction func didPressBack(_ sender: Any) {
+        self.dismiss(animated: true) {
+            //completion
+        }
     }
     
 
